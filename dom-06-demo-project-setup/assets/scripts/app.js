5eda0ed690ae60dd6aba35661da74a5f0eaa2c9f
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-10-27 23:48:30 +0200

    add modal function

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,17 +1,48 @@
 const modal = document.getElementById('add-modal');
 const btnAdd = document.getElementById('btn-add');
 const backDrop = document.getElementById('backdrop');
-
+const btnCancel= document.querySelector('.btn--passive');
+const btnAddMovie= document.querySelector('.btn--success');
+const userInputs = document.querySelectorAll('input');
 
 const toggleBackdrop=()=>{
     backDrop.classList.toggle('visible');
 }
 
-const openModal=()=>{
-    console.log('lala');
+const toggleModal=()=>{
     modal.classList.toggle('visible');
     toggleBackdrop();
 }
 
+const cancelAddMovieHandler=()=>{
+    toggleModal();
 
-btnAdd.addEventListener('click', openModal)
+}
+
+const addMovieHandler=()=>{
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1
+    +ratingValue > 5 
+    ){
+        alert('please enter value! (rating between 1 and 5)');
+        return
+    }
+
+
+}
+
+const backdropClickHandler = () =>{
+    toggleModal();
+}
+
+console.log(btnCancel)
+
+btnAdd.addEventListener('click', backdropClickHandler);
+btnCancel.addEventListener('click', cancelAddMovieHandler);
+btnAddMovie.addEventListener('click', addMovieHandler)
